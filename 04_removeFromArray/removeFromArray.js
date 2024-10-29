fcaefd947ba198999f5c2a8b5d026d679d7be447
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-29 19:08:53 +0000

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  let idx;
+  for (i = 1; i < arguments.length; i++) {
+    idx = arr.indexOf(arguments[i]);
+    while (arr.includes(arguments[i])) {
+      arr.splice(idx, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
