cf2943b29b715d05955b06e0f4e9baed4d4f352c
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-30 15:13:35 +0000

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let result = num1 + num2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let result = num1 - num2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  result = 0;
+  for (i = 0, len=array.length; i < len; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  if (array.length === 0) {
+    result = 0;
+  } else {
+    result = 1;
+  }
+  for (num in array) {
+    array[num] = +array[num];
+    result *= array[num];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
