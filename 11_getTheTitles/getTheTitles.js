1474dd2602caf9d381dd29bfd9234d9901561af2
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-30 17:56:41 +0000

    Complete getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+/*
+Return just the values of titles within the array.
+*/
+const getTheTitles = (array) => array.map((a) => a.title);
 
 // Do not edit below this line
 module.exports = getTheTitles;
