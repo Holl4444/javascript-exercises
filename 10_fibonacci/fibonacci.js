ae7d02a6ce70b3d67ead66b56d339807ba1ed3e5
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-30 16:59:30 +0000

    complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  // Accept strings
+  num = +num;
+  // negatives not valid
+  if (num < 0) {
+    return "OOPS";
+  }
+  // fib(0) == 0
+  if (num === 0) {
+    return 0;
+    // first 2 fibonnacis = 1
+  } else if (num < 3) {
+    return 1;
+  } else {
+    // sum of preceeding two fibonacci numbers
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 };
 
 // Do not edit below this line
