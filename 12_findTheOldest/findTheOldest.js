61573fb06c4dde6ef841b5bc75950860c2b5135a
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-30 18:48:17 +0000

    Update var names used with reduce() for clarity

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -6,8 +6,8 @@ const findTheOldest = function (list) {
     i.yearOfDeath ? i.yearOfDeath - i.yearOfBirth : thisYear - i.yearOfBirth
   );
   // Check through ages comparing focused element to each in the list and keeping only the largest
-  let max = ages.reduce((accumulator, currentValue) => {
-    return accumulator > currentValue ? accumulator : currentValue;
+  let max = ages.reduce((oldest, currentAge) => {
+    return oldest > currentAge ? oldest : currentAge;
   });
 
   //  Find the index of the oldest in age and return the object at that index of original list. ie the oldest person
