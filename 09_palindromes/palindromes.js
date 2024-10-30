a8599d1e773db5624f8038858d9507f9727e0d47
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-30 16:36:29 +0000

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let regex = /[\s\W]/g;
+  // replaces all occurences of whitespace, anything that isnt a
+  // letter, number or underscore within the string with empty string
+  // and sets all to lowercase.
+  str = str.replace(regex, "").toLowerCase();
+  // Compare the string to the string reversed. Matching = true
+  return str === str.split("").reverse().join("");
 };
 
 // Do not edit below this line
