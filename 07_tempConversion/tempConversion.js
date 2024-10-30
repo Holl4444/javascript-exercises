2c4cb32931c16f1f66daf3e13932433099ace062
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-30 13:22:22 +0000

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
+const DECIMAL = 1;
+
+function decimalPlace(float, places) {
+  let scale = Math.pow(10, places);
+  let numPlaced = Math.round(float * scale) / scale;
+  return numPlaced;
+}
+
+// °C = (°F - 32) × 5/9
+const convertToCelsius = function (degreesF) {
+  let degreesC = (degreesF - 32) * (5 / 9);
+  degreesC = decimalPlace(degreesC, DECIMAL);
+  return degreesC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesC) {
+  let degreesF = degreesC / (5 / 9) + 32;
+  degreesF = decimalPlace(degreesF, DECIMAL);
+  return degreesF;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
